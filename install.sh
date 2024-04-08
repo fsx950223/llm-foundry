4c425625e3ffa9e65ab4d3776e5520fda72d8b27
fsx950223 <fsx950223@outlook.com>
2024-04-08 09:26:07 +0000

    update deps

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-pip install --no-deps 'mosaicml[libcloud,wandb,mlflow,oci,gcs]>=0.16.3,<0.17'
 pip install -r requirements.txt
+pip install --no-deps 'mosaicml[libcloud,wandb,mlflow,oci,gcs]>=0.16.3,<0.17'
 python setup.py install
